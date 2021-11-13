cde9db8c5b4c84521dd3f1116ac4089602918e6d
magdychserg <magdych@mail.ru>
2021-11-13 21:04:15 +1200

    add views

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    return render(index, 'index.html')
+
+
+def products(request):
+    return render(products, 'products.html')
