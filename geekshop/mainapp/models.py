26a5933407bed57fa85de4d5a245599cce5dff69
magdychserg <magdych@mail.ru>
2021-11-18 23:46:45 +1200

    add media, admin

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -5,10 +5,16 @@ class ProductCategory(models.Model):
     name= models.CharField(max_length= 64)
     description= models.TextField(blank=True, null= True)
 
+def __str__(self):
+    return f'{self.name}'
+
 class Product(models.Model):
     name= models.CharField(max_length= 128)
     image= models.ImageField(upload_to='product_image', blank=True)
-    price= models.DateTimeField(max_digits= 8, decimal_place= 2)
+    price= models.DecimalField(max_digits=8, decimal_places=2)
     description= models.TextField(blank=True, null=True)
-    quantity= models.PositiveIntegerField(default= 0)
+    quantity= models.PositiveIntegerField(default=0)
     category= models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+
+def __str__(self):
+    return f'{self.name} | {self.category}'
