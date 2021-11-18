cf4f727869c84455b6e725548db0a3ffed61150f
magdychserg <magdych@mail.ru>
2021-11-18 21:50:40 +1200

    add json file and model

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class ProductCategory(models.Model):
+    name= models.CharField(max_length= 64)
+    description= models.TextField(blank=True, null= True)
+
+class Product(models.Model):
+    name= models.CharField(max_length= 128)
+    image= models.ImageField(upload_to='product_image', blank=True)
+    price= models.DateTimeField(max_digits= 8, decimal_place= 2)
+    description= models.TextField(blank=True, null=True)
+    quantity= models.PositiveIntegerField(default= 0)
+    category= models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+import json
+import os
 
+MODULE_DIR = os.path.dirname(__file__)
 
 # Create your views here.
 def index(request):
@@ -11,7 +14,10 @@ def index(request):
 
 
 def products(request):
+    file_path = os.path.join(MODULE_DIR, 'fixtures/products.json')
     context = {
         'title': 'Geekshop- catalog',
     }
+    context['products'] = json.load(open(file_path, encoding = 'utf-8'))
+
     return render(request, 'mainapp\products.html', context)
