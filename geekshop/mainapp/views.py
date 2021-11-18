e93e02befead65418690a282a52a84b571ca62fc
magdychserg <magdych@mail.ru>
2021-11-19 00:52:12 +1200

    add db in categories and product

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 import json
 import os
-
+from .models import Product, ProductCategory
 MODULE_DIR = os.path.dirname(__file__)
 
 # Create your views here.
@@ -14,10 +14,12 @@ def index(request):
 
 
 def products(request):
-    file_path = os.path.join(MODULE_DIR, 'fixtures/products.json')
+    #file_path = os.path.join(MODULE_DIR, 'fixtures/products.json')
     context = {
         'title': 'Geekshop- catalog',
+        'products': Product.objects.all(),
+        'productscategories' : ProductCategory.objects.all()
     }
-    context['products'] = json.load(open(file_path, encoding = 'utf-8'))
+   # context['products'] = json.load(open(file_path, encoding = 'utf-8'))
 
     return render(request, 'mainapp\products.html', context)
