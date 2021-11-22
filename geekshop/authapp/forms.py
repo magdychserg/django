7a9ad3faf2abcc080bcafc1cd14b009e8a8db869
magdychserg <magdych@mail.ru>
2021-11-22 22:22:38 +1200

    add login

--- geekshop/authapp/forms.py
+++ geekshop/authapp/forms.py
@@ -9,6 +9,13 @@ class UserLoginForm(AuthenticationForm):
         model = User
         fields = ('username', 'password')
 
+    def __init__(self, *args,**kwargs):
+        super(UserLoginForm,self).__init__(*args,**kwargs)
+        self.fields['username'].widget.attrs['placeholder']='Введите имя пользователя'
+        self.fields['password'].widget.attrs['placeholder'] = 'Введите  пароль'
+        for fields_name, fields in self.fields.items():
+            fields.widget.attrs['class']= 'form-control py-4'
+
 class UserRegisterForm(AuthenticationForm):
     pass
     # class Meta:
--- geekshop/authapp/views.py
+++ geekshop/authapp/views.py
@@ -1,9 +1,28 @@
+from django.contrib import auth
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
+from authapp.forms import UserLoginForm
+
+
 def login(request):
+    if request.method=='POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username= request.POST.get('username')
+            password = request.POST.get('password')
+            user= auth.authenticate(username=username, password=password)
+            if user.is_active:
+                auth.login(request,user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
     context = {
-        'title' : 'Geekshop | Login'
+        'title' : 'Geekshop | Login',
+        'form' : form
 
     }
     return render(request, 'authapp/login.html', context)
