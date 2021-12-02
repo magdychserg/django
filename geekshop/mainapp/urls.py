c2db4f16d9b5132f8813b56b6afcb0f7261826a6
magdychserg <magdych@mail.ru>
2021-12-02 23:27:06 +1200

    add detail

--- geekshop/mainapp/urls.py
+++ geekshop/mainapp/urls.py
@@ -1,10 +1,10 @@
-
 from django.urls import path
 
-from mainapp.views import index, products
+from mainapp.views import index, products, ProductDeatail
 
-app_name= 'products'
+app_name = 'products'
 urlpatterns = [
 
     path('products/', products, name='products'),
+    path('detail/<int:pk>/', ProductDeatail.as_view(), name='detail'),
 ]
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 import json
 import os
+
+from django.views.generic import DetailView
+
 from .models import Product, ProductCategory
 MODULE_DIR = os.path.dirname(__file__)
 
@@ -23,3 +26,13 @@ def products(request):
    # context['products'] = json.load(open(file_path, encoding = 'utf-8'))
 
     return render(request, 'mainapp\products.html', context)
+class ProductDeatail(DetailView):
+    model = Product
+    template_name = 'mainapp/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductDeatail, self).get_context_data(**kwargs)
+        product = self.get_object()
+        context['product'] = product
+        return context
+
