ad530df58b609c31f151840837244127991526c0
magdychserg <magdych@mail.ru>
2022-01-16 17:30:10 +1200

    add with

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -216,3 +216,15 @@ if DEBUG:
         'debug_toolbar.panels.profiling.ProfilingPanel',
         'template_profiler_panel.panels.template.TemplateProfilerPanel',
     ]
+CACHE_MIDDLEWARE_ALIAS = 'default'
+CACHE_MIDDLEWARE_SECONDS = 120
+CACHE_MIDDLEWARE_KEY_PREFIX = 'geekbrains'
+
+CACHES = {
+    'default':{
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '89.108.76.147:11211'
+    }
+}
+
+LOW_CACHE = True
