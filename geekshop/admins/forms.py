5dc085228f3a04a10ba9a200aa7b704acd888319
magdychserg <magdych@mail.ru>
2022-01-19 22:28:08 +1200

    add скидка

--- geekshop/admins/forms.py
+++ geekshop/admins/forms.py
@@ -36,9 +36,10 @@ class UserAdminProfileForm(UserProfilerForm):
 
 
 class ProductCategoryEditForm(forms.ModelForm):
+    discount = forms.IntegerField(widget=forms.NumberInput(),label='скидка', required=False, min_value=0,max_value=90,initial=0)
     class Meta:
         model = ProductCategory
-        fields = ('name', 'description', 'is_active')
+        fields = ('name', 'description', 'is_active', 'discount')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
--- geekshop/admins/views.py
+++ geekshop/admins/views.py
@@ -93,6 +93,7 @@ class CategoriesDeleteView(DeleteView, BaseClassContextMixin, CustomDispatchMixi
         self.object = self.get_object()
         if self.object.is_active:
             self.object.is_active = False
+            self.object.product_set.update(is_active=False)
         else:
             self.object.is_active = True
         self.object.save()
